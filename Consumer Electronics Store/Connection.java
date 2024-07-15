85f7917633457a971fb7c8c8d0186845b4731448
vinish-jkv <www.vinish18@gmail.com>
2024-07-15 23:03:15 +0530

    Update Connection.java

--- Consumer Electronics Store/Connection.java
+++ Consumer Electronics Store/Connection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 	public class JDBCdemo {  
 	    private static final String URL = "jdbc:mysql://localhost:3306/jdbcdemo";
-	    private static  String USER = "root";
-	    private static  String PASSWORD = "Jungook7$";
+	    private static  String USER = "*****";
+	    private static  String PASSWORD = "******";
 
 	    public static Connection getConnection() throws SQLException {
 	        return DriverManager.getConnection(URL, USER, PASSWORD);
